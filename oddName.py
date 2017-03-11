5319d66c6e5c8431d7ab79bcf40a1b597fa22e0f
Oliver <oliver.atkinson@my.jcu.edu.au>
2017-03-11 15:31:06 +1000

    Add name printing program

--- oddName.py
+++ oddName.py
@@ -1,1 +1,7 @@
 '''Oliver Atkinson'''
+name = str(input("What is your name"))
+if name == "":
+    print("error")
+    str(input("What is your name"))
+for i in range(0,len(name),2):
+    print(name[i+1],end=' ')
